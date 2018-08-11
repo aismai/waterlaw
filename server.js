5bc24275c41b38b0f1ccd1c5d7e133e1a38ae24c
aismai <abai.ismailov@gmail.com>
2018-08-11 23:45:41 +0600

    refactor server to express

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-type": "text/html" });
-    let HTML = fs.readFileSync("./index.html");
-    res.end(HTML);
-  } else if (req.url === "/api/user") {
-    res.writeHead(200, { "Content-type": "application/json" });
-    const json = JSON.stringify({
-      name: "Annie",
-      cars: ["BMW", "Lexus"]
-    });
-
-    res.end(json);
-  } else {
-    res.writeHead(400);
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.send("Are you ok, Annie?");
 });
 
-server.listen(3003, "127.0.0.1");
+app.get("/api/user", (req, res) => {
+  res.send({
+    name: "Annie",
+    cars: ["BMW", "Lexus"]
+  });
+});
+
+const port = process.env.PORT || 3003;
+
+app.listen(port);
