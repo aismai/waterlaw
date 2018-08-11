8dcf3c980ee169e75028d0f2a753a1be6ab9d1ae
aismai <abai.ismailov@gmail.com>
2018-08-11 23:24:58 +0600

    add routes

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/html" });
-  res.end("Annie are you ok?");
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    let HTML = fs.readFileSync("./index.html");
+    res.end(HTML);
+  } else if (req.url === "/api/user") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    const json = JSON.stringify({
+      name: "Annie",
+      cars: ["BMW", "Lexus"]
+    });
+
+    res.end(json);
+  } else {
+    res.writeHead(400);
+    res.end();
+  }
 });
 
 server.listen(3003, "127.0.0.1");
