5be05dbeed9aa7bfdb2644d43b2e27d60bddd241
aismai <abai.ismailov@gmail.com>
2018-08-12 15:14:47 +0600

    refactor routes endpoints for react

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
 const express = require("express");
-const querystring = require("querystring");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Are you ok, Annie?");
+app.get("/api/users", (req, res) => {
+  res.json([{ id: 1, username: "Annie " }, { id: 2, username: "Blair" }]);
 });
 
-app.get("/api/user", (req, res) => {
-  res.send({
-    name: "Annie",
-    cars: ["BMW", "Lexus"]
-  });
+app.get("/api/cars", (req, res) => {
+  res.json([{ id: 1, brand: "Audi" }, { id: 2, brand: "BMW" }]);
 });
 
-app.get("/api/user/:id", (req, res) => {
-  let id = req.params.id;
-  res.send(`The user id is: ${id}`);
-});
-
-app.get("/api/car", (req, res) => {
-  let brand = req.query.brand;
-  let year = req.query.year;
-  res.send({ brand, year });
-});
 const port = process.env.PORT || 3003;
 
 app.listen(port);
