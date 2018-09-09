1c5012ef86f03c490a34fd561a688fc1afcdc315
aismai <abai.ismailov@gmail.com>
2018-09-09 15:24:54 +0600

    add Wood model

--- server/server.js
+++ server/server.js
@@ -20,10 +20,33 @@ app.use(cookieParser());
 // MODELS
 const { User } = require("./models/user");
 const { Brand } = require("./models/brand");
+const { Wood } = require("./models/wood");
 
 // MIDDLEWARE
 const { auth } = require("./middleware/auth");
 const { admin } = require("./middleware/admin");
+
+// WOODS ROUTES
+app.post("/api/product/wood", auth, admin, (req, res) => {
+  const wood = new Wood(req.body);
+
+  wood.save((err, doc) => {
+    if (err) return res.json({ success: false });
+
+    res.status(200).json({
+      success: true,
+      wood: doc
+    });
+  });
+});
+
+app.get("/api/product/woods", (req, res) => {
+  Wood.find({}, (err, woods) => {
+    if (err) return res.status(400).send(err);
+
+    res.status(200).send(woods);
+  });
+});
 
 // BRAND ROUTES
 app.post("/api/product/brand", auth, admin, (req, res) => {
