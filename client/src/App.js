738223a5dc4c928472102c5a8afb468c3277a145
aismai <abai.ismailov@gmail.com>
2018-09-11 18:28:48 +0600

    add client dependencies
    remove generated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 import axios from "axios";
 
 class App extends Component {
-  componentWillMount() {
-    axios.get("/api/users").then(response => console.log(response.data));
-    axios.get("/api/cars").then(response => console.log(response.data));
+  componentDidMount() {
+    axios.get("/api/product/brands").then(response => console.log(response));
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
