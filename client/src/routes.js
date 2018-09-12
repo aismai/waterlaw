fda0940ec938b211d26246f8c844076d9cf2c5df
aismai <abai.ismailov@gmail.com>
2018-09-12 19:11:21 +0600

    add Header and Footer

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import Layout from "./components/hoc/layout";
 import Home from "./components/Home";
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Layout>
   );
 };
 
